e52d1be8ee457c879eb75d0c225218dfe58100d5
schwarzam <oliveira@oliveiras-MacBook-Pro.local>
2020-10-19 10:01:47 -0300

    0.76, fixed searchcoord by ra and dec

--- splusdata/searchcoords.py
+++ splusdata/searchcoords.py
@@ -12,6 +12,33 @@ class searchcoords:
     self.dec = dec
     self.password = password
 
+  def search_survey(self):
+    ra = self.ra
+    dec = self.dec
+    password = self.password
+
+    if not self.password:
+        password = str(input("input password: "))
+    engine = sqlalchemy.create_engine(f'postgresql://SPLUS_readonly:{password}@splus.cloud:5432/splus')
+
+    query = f"""SELECT "RA", "DEC", "NAME" FROM "Ref" WHERE "RA" < {ra+7} and "RA" > {ra - 7} and "DEC" > {dec -7} and "DEC" < {dec + 7}"""
+    Galaxy = pd.read_sql_query(query, engine)
+
+    ra2 = Galaxy.RA.to_numpy()
+    dec2 = Galaxy.DEC.to_numpy()
+
+    c = SkyCoord(ra=ra*u.degree, dec=dec*u.degree)
+    catalog = SkyCoord(ra=ra2*u.degree, dec=dec2*u.degree)
+    idx, d2d, d3d = c.match_to_catalog_sky(catalog)
+    idx2 = idx
+
+    Field = Galaxy.NAME[idx2]
+
+    df = pd.read_csv('https://raw.githubusercontent.com/Schwarzam/Data-analyse---SPLUS-objs/master/fields.csv')
+    Galaxy = df[df['NAME'] == Field]
+
+    return Galaxy.SUBREGION.to_numpy()[0].lower()
+
   def search_field(self):
     ra = self.ra
     dec = self.dec
@@ -58,7 +85,7 @@ class searchcoords:
 
     Field = Galaxy.NAME[idx2]
 
-    whereField = pd.read_csv('https://raw.githubusercontent.com/Schwarzam/Data-analyse---SPLUS-objs/master/fields.csv', skiprows=1)
+    whereField = pd.read_csv('https://raw.githubusercontent.com/Schwarzam/Data-analyse---SPLUS-objs/master/fields.csv')
     ans = whereField[whereField['NAME'] == Field]
 
     if len(ans) < 1:
