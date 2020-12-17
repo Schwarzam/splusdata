a6a204cab5c7ca17fa28083064a4d539706cc699
Gustavo Schwarz <gustavo.b.schwarz@gmail.com>
2020-12-17 14:29:13 -0300

    Update get_fits.py

--- splusdata/get_fits.py
+++ splusdata/get_fits.py
@@ -12,22 +12,22 @@ from astropy import units as u
 def get_fits(name, ra, dec, cut_size, band, filename=None, overwrite=False):
     warnings.simplefilter("ignore")
 
-    response = urlopen(f'http://splus.cloud:8000/api/{name}/{ra}/{dec}/{cut_size}/{band}/0')
+    response = urlopen(f'http://splus.cloud/api/{name}/{ra}/{dec}/{cut_size}/{band}/0')
     name_in_query = response.read().decode('utf-8')
 
-    response = urlopen(f'http://splus.cloud:8000/api/{name_in_query}/{ra}/{dec}/{cut_size}/{band}/1')
+    response = urlopen(f'http://splus.cloud/api/{name_in_query}/{ra}/{dec}/{cut_size}/{band}/1')
     queue = response.read().decode('utf-8')
 
     while int(queue) != 0:
         time.sleep(5)
-        response = urlopen(f'http://splus.cloud:8000/api/{name_in_query}/{ra}/{dec}/{cut_size}/{band}/1')
+        response = urlopen(f'http://splus.cloud/api/{name_in_query}/{ra}/{dec}/{cut_size}/{band}/1')
         string = response.read().decode('utf-8')
 
         queue = int(string)
         print('you are in position:', string, 'in queue')
 
     if int(queue) == 0:
-        response = urlopen(f'http://splus.cloud:8000/api/{name_in_query}/{ra}/{dec}/{cut_size}/{band}/2')
+        response = urlopen(f'http://splus.cloud/api/{name_in_query}/{ra}/{dec}/{cut_size}/{band}/2')
         queue = response.read().decode('utf-8')
 
         new_hdu = fits.PrimaryHDU()
