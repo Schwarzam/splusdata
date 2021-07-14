a004941471697e3d7cdfbf5b30b7b7eb6dd6fba5
Erik V <erik-vini@hotmail.com>
2021-07-14 19:06:57 -0300

    Update pdfs.py
    Added command to reset DF indexes before loop.
    Changed 'fromstring' to get the [0:] elements from each PDF feature, instead of [1:-1] (the last one was used because the values were saved with [ and ]

--- splusdata/vacs/pdfs.py
+++ splusdata/vacs/pdfs.py
@@ -6,11 +6,12 @@ def Calc_PDF(x, Weights, Means, STDs):
     PDF = np.sum(Weights*(1/(STDs*np.sqrt(2*np.pi))) * np.exp((-1/2) * ((x[:,None]-Means)**2)/(STDs)**2), axis=1)
     return PDF/np.trapz(PDF, x)
 
-def Calculate_PDFs(PDF_Catalogue, x=np.arange(0, 1, 0.001)):
+def Calculate_PDFs(PDF_Catalogue, x=np.arange(0, 1.001, 0.001)):
     Final_PDFs = []
+    PDF_Catalogue = PDF_Catalogue.reset_index(drop=True)
     for i in range(len(PDF_Catalogue)):
-        Final_PDFs.append(Calc_PDF(x, np.fromstring(PDF_Catalogue['PDF_Weights'][i][1:-1], sep=','), 
-                                      np.fromstring(PDF_Catalogue['PDF_Means'][i][1:-1], sep=','), 
-                                      np.fromstring(PDF_Catalogue['PDF_STDs'][i][1:-1], sep=',')))
+        Final_PDFs.append(Calc_PDF(x, np.fromstring(PDF_Catalogue['PDF_Weights'][i][0:], sep=','), 
+                                      np.fromstring(PDF_Catalogue['PDF_Means'][i][0:], sep=','), 
+                                      np.fromstring(PDF_Catalogue['PDF_STDs'][i][0:], sep=',')))
 
     return x, Final_PDFs
