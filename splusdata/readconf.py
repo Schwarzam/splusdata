f366e636148b76d4d0b2c59f36c63941aad50949
Vincenzo <alberice.vincenzo@gmail.com>
2023-12-04 13:02:32 -0300

    adding fieldframe and checkcoord operatio

--- splusdata/readconf.py
+++ splusdata/readconf.py
@@ -16,9 +16,9 @@ operations = [
     'stamp',
     'lupton_rgb',
     'trilogy_image'
-    #'field_frame',
-    #'checkcoords',
-    #'query'
+    'field_frame',
+    'checkcoords',
+    'query'
 ]
 
 def filter_info(info, function):
@@ -39,6 +39,10 @@ def create_stamp_name(info, output_folder):
 
 def create_image_name(info, output_folder):
     filename = os.path.join(output_folder, f"{info['ra']}_{info['dec']}_{info['size']}.png")
+    return filename
+
+def create_field_name(info, output_folder):
+    filename = os.path.join(output_folder, f"{info['field']}_{info['band']}.fits")
     return filename
     
 def handle_operation(operation : str, info, conn):
@@ -61,6 +65,40 @@ def handle_operation(operation : str, info, conn):
         info = filter_info(info, conn.trilogy_image)
         conn.trilogy_image( filename=filename, **info )
 
+    elif operation == 'field_frame':
+        filename = create_field_name(info, output_folder)
+        info = filter_info(info, conn.field_frame)
+        conn.field_frame( filename=filename, **info )
+
+    elif operation == 'checkcoords':
+        info = filter_info(info, conn.checkcoords)
+        try:
+            data = conn.checkcoords( **info )
+            data["ra"] = info["ra"]
+            data["dec"] = info["dec"]
+        except:
+            data = {"ra": info["ra"], "dec": info["dec"], "field": None, "distance": None, "public": None, "internal": None}
+        return data
+    elif operation == 'query':
+        if "query" not in info:
+            raise ValueError("Query not specified")
+        if "upload_file" in info:
+            if not os.path.exists(info["upload_file"]):
+                raise ValueError("File {} does not exist".format(info["upload_file"]))
+            info["query"] = open(info["upload_file"], "r").read()
+        else:
+            filename = os.path.join(output_folder, "query.fits")
+            info = filter_info(info, conn.query)
+            try:
+                data = conn.query( **info )
+                data.write(filename, overwrite=True)
+            except Exception as e:
+                print(e)
+            
+
+
+    
+
 def get_coordinates_col(df : pd.DataFrame):
     ra = None
     dec = None
@@ -78,6 +116,7 @@ def get_coordinates_col(df : pd.DataFrame):
             
 
 def handle_operation_type(operation_data, conn):
+    checkcoords_list = []
     if isinstance(operation_data["type"], str):
         operations = [operation_data["type"]]
     elif isinstance(operation_data["type"], list):
@@ -96,18 +135,29 @@ def handle_operation_type(operation_data, conn):
             df = Table.read(operation_data["file_path"]).to_pandas()
 
         ra_col, dec_col = get_coordinates_col(df)
-        
+
         for key, value in df.iterrows():
             info = operation_data
             ## Add ra and dec to info
             info["ra"] = value[ra_col]
             info["dec"] = value[dec_col]
             for op in operations:
-                handle_operation(op, info, conn)
+                if op == "checkcoords":
+                    checkcoords_list.append(handle_operation(op, info, conn))
+                else:
+                    handle_operation(op, info, conn)
     
     else:
         for op in operations:
-            handle_operation(op, operation_data, conn)
+            if op == "checkcoords":
+                checkcoords_list.append(handle_operation(op, operation_data, conn))
+            else:
+                handle_operation(op, operation_data, conn)
+
+    if len(checkcoords_list) > 0:
+        checkcoords_df = pd.DataFrame(checkcoords_list)
+        checkcoords_df.to_csv(os.path.join(operation_data["output_folder"], "checkcoords.csv"), index=False)
+
 
 
 def main():
@@ -126,6 +176,8 @@ def main():
         conn = splusdata.Core()
         
     handle_operation_type(data["operation"], conn)
+
+    return 0
 
         
 
